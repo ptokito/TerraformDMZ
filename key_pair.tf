4779754c41756130fb3d4cba04f2d19b528f98f8
Tim Okito <62076924+ptokito@users.noreply.github.com>
2020-10-29 14:15:36 -0400

    Update key_pair.tf

--- key_pair.tf
+++ key_pair.tf
@@ -7,14 +7,4 @@ resource "tls_private_key" "key1" {
 	]
 }
 
-#resource "local_file" "private-key" {
-#	content         = "tls_private_key.tfkey.private_key_pem"
-#	filename        = "main_key.pem" 
- #"aws_key_pair" "deployer"
-#	key_name   = "key"
-#	public_key = tls_private_key.tfkey.public_key_openssh
-#	depends_on = [
-#		tls_private_key.key1
-#	]
-    
-#}
+
