5119cb82bbf8e3c0df1cafabdc4e5c76fcd78569
Tim Okito <62076924+ptokito@users.noreply.github.com>
2020-10-29 14:18:13 -0400

    Update web_to_key.tf

--- web_to_key.tf
+++ web_to_key.tf
@@ -6,13 +6,3 @@ resource "null_resource" "key_update" {
     host        = aws_instance.web.public_ip 
   }
 }
- # provisioner "file" {
-  #  source = ".fkey.pem"
-   # destination = "C:\Users\t430\Desktop\AWS_DMZ\private_key.pem"
-  #}
-
-  #depends_on = [
-    
-   # null_resource.update
-  #]
-#}
