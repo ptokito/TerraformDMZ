a79e9e6c8683a6357560e175ea252ff72e486520
Tim Okito <62076924+ptokito@users.noreply.github.com>
2020-11-03 14:11:49 -0500

    Update ec2_web.tf

--- ec2_web.tf
+++ ec2_web.tf
@@ -1,6 +1,6 @@
   
 resource "aws_instance" "web" {
-	ami           = "ami-0947d2ba12ee1ff75" #region specific
+	ami           = "ami-0947d2ba12ee1ff75"
 	instance_type = "t2.micro"
 	subnet_id 	  = aws_subnet.publicsubnet.id 
 	user_data = <<EOF
